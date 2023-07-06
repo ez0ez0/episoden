82a333f656ba5e37d8f201d55d5e7be8e0ad56a3
ez0ez0 <89349685+ez0ez0@users.noreply.github.com>
2023-07-07 00:11:21 +0900

    feat: initial setting

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "singleQuote": false,
+    "semi": true,
+    "useTabs": false,
+    "tabWidth": 4,
+    "trailingComma": "all",
+    "printWidth": 160,
+    "arrowParens": "avoid",
+    "endOfLine": "auto"
+};
